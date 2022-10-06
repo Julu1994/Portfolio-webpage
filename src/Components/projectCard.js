38a1c84952550c623a15e52e5d9886d4bce2bcf3
julu1994 <mrjewel837@gmail.com>
2022-10-07 00:20:03 +0200

    completed the project section

--- src/Components/projectCard.js
+++ src/Components/projectCard.js
@@ -3,31 +3,55 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
+import { IconButton } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { AiFillGithub } from "react-icons/ai";
+import { MdLiveTv } from "react-icons/md";
 
-const ProjectCard = () => {
+const ProjectCard = (props) => {
+    const { name, details, url, demo, github } = props;
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <Card
+            sx={{
+                maxWidth: 225,
+                borderTopRightRadius: "2rem",
+                borderBottomLeftRadius: "2rem",
+                backgroundColor: "#F8F8FF",
+            }}>
             <CardMedia
                 component="img"
-                height="140"
-                image="/static/images/cards/contemplative-reptile.jpg"
+                height="120"
+                image={url}
                 alt="green iguana"
             />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Lizard
+            <CardContent sx={{ mt: "0", pt: "0", pb: "0", mb: "0" }}>
+                <Typography
+                    gutterBottom
+                    variant="h6"
+                    component="div"
+                    sx={{ mt: "0", mb: "0", pt: "0", pb: "0" }}>
+                    {name}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
+                <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    sx={{ mt: "0", pt: "0" }}>
+                    {details}
                 </Typography>
             </CardContent>
-            <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+            <CardActions
+                sx={{ display: "flex", justifyContent: "space-around" }}>
+                <IconButton size="small">
+                    <a href={github}>
+                        <AiFillGithub size={25} color="#FFA500" />
+                    </a>
+                </IconButton>
+
+                <IconButton size="small">
+                    <a href={demo}>
+                        <MdLiveTv size={25} color="#FFA500" />
+                    </a>
+                </IconButton>
             </CardActions>
         </Card>
     );
--- src/Pages/projectPage.js
+++ src/Pages/projectPage.js
@@ -2,13 +2,42 @@ import "./projectPage.scss";
 import React from "react";
 import { Bubble } from "../Components/bubble";
 import SectionTitle from "../Components/sectionTitle";
+import { data } from "../Components/projectData";
+import ProjectCard from "../Components/projectCard";
 
 const ProjectPage = () => {
     return (
         <div className="projects">
-            Projects
             <div className="projects-item"></div>
             <SectionTitle text={"Projects"} />
+            <div className="projects-top">
+                {data.slice(0, 2).map((item) => {
+                    return (
+                        <ProjectCard
+                            key={item.github}
+                            name={item.name}
+                            details={item.details}
+                            url={item.img}
+                            demo={item.demo}
+                            github={item.github}
+                        />
+                    );
+                })}
+            </div>
+            <div className="projects-bottom">
+                {data.slice(2, 4).map((item) => {
+                    return (
+                        <ProjectCard
+                            key={item.github}
+                            name={item.name}
+                            details={item.details}
+                            url={item.img}
+                            demo={item.demo}
+                            github={item.github}
+                        />
+                    );
+                })}
+            </div>
             <Bubble />
         </div>
     );
